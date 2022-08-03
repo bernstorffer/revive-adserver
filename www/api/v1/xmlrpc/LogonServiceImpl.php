7b5c52a2a474df575b80432b9c791c2227fab5d1
lcuis <louis.villedieu@gmail.com>
2022-08-03 23:38:56 +0100

    Call badLogin.php when login failed
    in support for fail2ban

--- www/api/v1/xmlrpc/LogonServiceImpl.php
+++ www/api/v1/xmlrpc/LogonServiceImpl.php
@@ -109,6 +109,7 @@ class LogonServiceImpl extends BaseServiceImpl
                 return false;
             }
         } else {
+            require_once MAX_PATH . '/lib/badLogin.php';
             $this->raiseError($strUsernameOrPasswordWrong);
             return false;
         }
