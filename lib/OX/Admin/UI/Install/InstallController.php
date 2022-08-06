ca8c3f0e9896401729455b8d40202f9c4775ab30
lcuis <louis.villedieu@gmail.com>
2022-08-06 20:46:04 +0100

    badLogin moved

--- lib/OX/Admin/UI/Install/InstallController.php
+++ lib/OX/Admin/UI/Install/InstallController.php
@@ -306,7 +306,7 @@ class OX_Admin_UI_Install_InstallController extends OX_Admin_UI_Controller_BaseC
 
         if ($oForm->validate()) {
             if (!OA_Upgrade_Login::checkLogin()) {
-                require_once MAX_PATH . '/lib/badLogin.php';
+                require_once MAX_PATH . '/lib/max/other/badLogin.php';
                 $this->setModelProperty(
                     'aMessages',
                     ['error' => [$GLOBALS['strUsernameOrPasswordWrong']]]
